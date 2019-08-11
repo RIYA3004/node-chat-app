a4d52a227df3bacae7966208958039086a0ec6db
RIYA3004 <riya.suri@somaiya.edu>
2019-08-11 17:10:40 +0530

    Modification in  json the correct one

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 const path=require('path');
 const express =require('express');
+
 const publicPath =path.join(__dirname,'../public');
-const port =process.env.PORT || 3000;
+
 // __dirname is the rro tname
 var app =express();
 app.use(express.static(publicPath));
 // the static is the methid to fire up static files  like html,css, and javascript
 
-app.listen(port, () => {
-  console.log('Server is up on ${port}');
+app.listen(3000, () => {
+  console.log('Server is up on ');
 });

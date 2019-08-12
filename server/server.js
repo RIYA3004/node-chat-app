9d6c562782146cc077048500b97659f7e59d3a09
RIYA3004 <riya.suri@somaiya.edu>
2019-08-12 08:51:39 +0530

    Added connect and disconnect event handler

--- server/server.js
+++ server/server.js
@@ -1,13 +1,29 @@
 const path=require('path');
+const http=require('http');//inbuilt
 const express =require('express');
-
+const socketIO=require('socket.io');
 const publicPath =path.join(__dirname,'../public');
 const port =process.env.PORT || 3000;
 // __dirname is the rro tname
 var app =express();
+var server=http.createServer(app);
+var io =socketIO(server);
+
 app.use(express.static(publicPath));
 // the static is the methid to fire up static files  like html,css, and javascript
+io.on('connection',(socket)=>{
+  console.log('New User Connected');
+  socket.on('disconnect',()=>{
+    console.log("User disconnected");
+  });
+});
 
-app.listen(port, () => {
+
+
+//listens to new connection
+//app.listen(port, () => {//app.create calls http.create server actually
+  //console.log('Server is up on ');
+//});
+server.listen(port, () => {//app.create calls http.create server actually
   console.log('Server is up on ');
 });
